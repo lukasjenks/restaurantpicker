0897df7e12eddbe05ea176c2965f1fda37a721c0
Lukas Jenks <lukasjenks@gmail.com>
2019-07-19 18:32:05 -0600

    Fix segfault due to retardation

--- picker.c
+++ picker.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-void getRestaurants(char ***restaurantsArray) {
-    FILE *restaurantsFile = fopen("/home/lukas/restaurants.txt", "r");
-    char *restaurant = (char *)malloc(100 * sizeof(char));
+int insertIndex = 0;
+
+void getRestaurantsFromFile(char **restaurantsArray) {
+    FILE *restaurantsFile = fopen("./restaurants.txt", "r");
+    char *restaurant = (char *)malloc(50 * sizeof(char));
     char *restaurantCopy = restaurant;
-    restaurantCopy[0] = '\0';
 
     //fopen will return null if it is unable to read the file
     if (restaurantsFile == NULL) {
@@ -13,28 +15,25 @@ void getRestaurants(char ***restaurantsArray) {
 	return;
     }
     
-    int index = 0;
     while (fgets(restaurantCopy, 50, restaurantsFile)) {
-	// printf("%s\n%s\n", typeof(restaurantsArray[index]), typeof(restaurantCopy));
-	restaurantsArray[index] = restaurantCopy;
+	restaurantsArray[insertIndex] = (char*)malloc(50 * sizeof(char));
+	strcpy(restaurantsArray[insertIndex], restaurantCopy);
 	printf("%s", restaurantCopy);
-	printf("%s", restaurantsArray[index]);
-	index++;
+	printf("%s", restaurantsArray[insertIndex]);
+	insertIndex++;
     }
 
     fclose(restaurantsFile);
-    // free(restaurant);
+    free(restaurant);
 }
 
-void main() {
-    printf("---- FAT FRIDAY PICKER ----\n");
+void main(int argc, char **argv) {
+    // parse command line args, if exist
+    
+
+    // initialize restaurant array and populate it with restaurants from the file
     char **restaurantsArray = (char **)malloc(100 * sizeof(char *));
     char **restaurantsArrayCopy = restaurantsArray;
-    getRestaurants(&restaurantsArrayCopy);
-    printf("%s", restaurantsArrayCopy[0]);
-    // for (int i = 0; i < sizeof(restaurantsArray); i++) {
-    // printf("%s\n", restaurantsArray[i]);
-    // }
-    // free(restaurantsArray);
+    getRestaurantsFromFile(restaurantsArrayCopy);
+    free(restaurantsArray);
 }
-
