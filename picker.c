5a8a92e3b6cad7bc2b728c3d14c12d65b0901bc7
Lukas Jenks <lukasjenks@gmail.com>
2019-07-19 19:13:00 -0600

    Implement basic random selection of restaurant based on text file contents

--- picker.c
+++ picker.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 int insertIndex = 0;
+
+int getRandom(int min, int max){
+   return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+}
 
 void getRestaurantsFromFile(char **restaurantsArray) {
     FILE *restaurantsFile = fopen("./restaurants.txt", "r");
@@ -18,8 +23,6 @@ void getRestaurantsFromFile(char **restaurantsArray) {
     while (fgets(restaurantCopy, 50, restaurantsFile)) {
 	restaurantsArray[insertIndex] = (char*)malloc(50 * sizeof(char));
 	strcpy(restaurantsArray[insertIndex], restaurantCopy);
-	printf("%s", restaurantCopy);
-	printf("%s", restaurantsArray[insertIndex]);
 	insertIndex++;
     }
 
@@ -27,13 +30,18 @@ void getRestaurantsFromFile(char **restaurantsArray) {
     free(restaurant);
 }
 
-void main(int argc, char **argv) {
-    // parse command line args, if exist
-    
-
+void main() {
     // initialize restaurant array and populate it with restaurants from the file
     char **restaurantsArray = (char **)malloc(100 * sizeof(char *));
     char **restaurantsArrayCopy = restaurantsArray;
     getRestaurantsFromFile(restaurantsArrayCopy);
+    // seed rand function with current time
+    srand (time(NULL));
+    // pick a restaurant
+    printf("The restaurant you will go to is: %s", restaurantsArrayCopy[rand() % insertIndex]);
+    // free allocated memory
+    for (int i=0; i<=insertIndex; i++) {
+	free(restaurantsArray[i]);
+    }
     free(restaurantsArray);
 }
