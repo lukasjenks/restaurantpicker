ee09fd75e278311f11a20e07f37a6b4caa45b86b
Lukas Jenks <lukasjenks@gmail.com>
2019-07-20 14:30:50 -0600

    Implement better usability through continual input processing

--- picker.c
+++ picker.c
@@ -25,6 +25,10 @@ int getRestaurantsFromFile(char **restaurantsArray) {
     else {   
         while (fgets(restaurantCopy, 50, restaurantsFile)) {
             restaurantsArray[insertIndex] = (char*)malloc(50 * sizeof(char));
+            // replace newline at end of string with null terminator if exist
+            if (restaurantCopy[strlen(restaurantCopy)-1] == '\n') {
+                restaurantCopy[strlen(restaurantCopy)-1] = '\0';
+            }
             strcpy(restaurantsArray[insertIndex], restaurantCopy);
             insertIndex++;
         }
@@ -38,14 +42,37 @@ void main() {
     // initialize restaurant array and populate it with restaurants from the file
     char **restaurantsArray = (char **)malloc(100 * sizeof(char *));
     char **restaurantsArrayCopy = restaurantsArray;
-    if (getRestaurantsFromFile(restaurantsArrayCopy) == 0) {
-        // pick a restaurant
-        printf("The restaurant you will go to is %s", restaurantsArrayCopy[getRandom(0, insertIndex-1)]);
-        // free allocated memory
-    } 
-    else {
-        printf("No restaurants.txt file exists in this directory.\n");
+
+    if (getRestaurantsFromFile(restaurantsArrayCopy) != 0) {
+        printf("No restaurants.txt file exists in this directory. Please create it, then run this program again.");
+        for (int i=0; i<=insertIndex; i++) {
+	        free(restaurantsArray[i]);
+        }
+        free(restaurantsArray);
+        return;
     }
+
+    printf("Enter a command. For a list of commands available, enter 'h'\n");
+    char *input = (char *)malloc(50 * sizeof(char *));
+    char *inputCopy = input;
+
+    // main loop to process user input
+    while (fgets(inputCopy, 50, stdin)) {
+        inputCopy[strlen(inputCopy)-1] = '\0';
+        if (strcmp(inputCopy, "h") == 0) {
+            printf("COMMANDS:\na - add a restaurant\np - pick a restaurant\nq - close the program\n");
+        }
+        else if (strcmp(inputCopy, "p") == 0) {
+            printf("The restaurant you will go to is %s.\n", restaurantsArrayCopy[getRandom(0, insertIndex-1)]);
+        }
+        else if (strcmp(inputCopy, "q") == 0) {
+            printf("Exiting program...\n");
+            break;
+        }
+    }
+
+    // Free allocated memory
+    free(input);
     for (int i=0; i<=insertIndex; i++) {
 	    free(restaurantsArray[i]);
     }
