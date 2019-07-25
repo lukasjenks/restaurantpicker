d3f54fd330f796487410a6b7e617f006fffa64c5
Lukas Jenks <lukasjenks@gmail.com>
2019-07-24 20:36:31 -0600

    Implement removal of restaurants during runtime functionality

--- picker.c
+++ picker.c
@@ -6,12 +6,6 @@
 
 int insertIndex = 0;
 
-static void sigint_handler(int signal, siginfo_t * t, void *arg) {
-    printf("\n\nYou're not allowed to kill my program :)\n");
-    // pid_t pid = getpid();
-    // kill(pid, SIGKILL);
-}
-
 int getRandom(int min, int max) {
     return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
@@ -58,15 +52,6 @@ int main() {
         return 1;
     }
 
-    // struct to define behaviour for sigint signals (changes default behaviour of SIGINT
-    // encounter to be to call the isgint_handler function)
-    struct sigaction sa_sigint;
-    memset(&sa_sigint, 0, sizeof(sa_sigint));
-    sigemptyset(&sa_sigint.sa_mask);
-    sa_sigint.sa_sigaction = sigint_handler;
-    sa_sigint.sa_flags = SA_SIGINFO;
-    sigaction(SIGINT, &sa_sigint, NULL);
-
     printf("\nEnter a command. For a list of commands available, enter 'h'\n\n");
     char *input = (char *)malloc(50 * sizeof(char *));
     char *inputCopy = input;
@@ -75,20 +60,24 @@ int main() {
     while (1) {
 	printf("> ");
 	fgets(inputCopy, 50, stdin);
-        inputCopy[strlen(inputCopy)-1] = '\0';
+	inputCopy[strlen(inputCopy)-1] = '\0';
+	// Display help message mode
         if (strcmp(inputCopy, "h") == 0) {
             printf("\nCOMMANDS:\na - add a restaurant\np - pick a restaurant\nq - close the program\n\n");
         }
-	
+	// Add restaurant mode
 	else if (strcmp(inputCopy, "a") == 0) {
 	    FILE *restaurantsFile = fopen("./restaurants.txt", "a");
 	    char *restaurant = (char *)malloc(50 * sizeof(char));
 	    char *restaurantCopy = restaurant;
 	    printf("\nEnter the name of the restaurant you would like to add:\n\n");
 	    fgets(restaurantCopy, 50, stdin);
+	    // Replace newline at end of input string with null terminator
+	    restaurantCopy[strlen(restaurantCopy)-1] = '\0';	    
 
-	    //fopen will return null if it is unable to read the file
+	    // fopen will return null if it is unable to read the file
 	    if (restaurantsFile == NULL) {
+		printf("\nAn error occured. Most likely, restaurants.txt does not exist.\n\n");		
 		fclose(restaurantsFile);
 		free(restaurant);
 		break;
@@ -104,10 +93,65 @@ int main() {
 	    fclose(restaurantsFile);
 	    free(restaurant);
 	}
-		
+	// Remove restaurant mode
+	else if (strcmp(inputCopy, "r") == 0) {
+	    FILE *restaurantsFile = fopen("./restaurants.txt", "r");
+	    char *restaurant = (char *)malloc(50 * sizeof(char));
+	    char *restaurantCopy = restaurant;
+	    printf("\nEnter the name of the restaurant you would like to remove:\n\n");
+	    fgets(restaurantCopy, 50, stdin);
+
+	    // fopen will return null if it is unable to read the file
+	    if (restaurantsFile == NULL) {
+		printf("\nAn error occured. Most likely, restaurants.txt does not exist.\n\n");
+		fclose(restaurantsFile);
+		free(restaurant);
+		break;
+	    }
+
+	    // Replace newline at end of input string with null terminator for comparison
+	    restaurantCopy[strlen(restaurantCopy)-1] = '\0';
+
+	    
+	    int found = 0;
+	    // remove from array
+	    for (int index = 0; index < insertIndex; index++) {
+		if (strcmp(restaurantsArrayCopy[index], restaurantCopy) == 0) {
+		    found = 1;
+		    // if element to remove is simply last element, free memory and decrease insertIndex
+		    if (index == (insertIndex - 1)) {
+			free(restaurantsArray[insertIndex - 1]);
+			insertIndex--;
+		    }
+		    // else we need to swap with last element, and free memory of last index and derease insertIndex
+		    else {
+			strcpy(restaurantsArrayCopy[index], restaurantsArrayCopy[insertIndex-1]);
+			free(restaurantsArray[insertIndex-1]);
+			insertIndex--;
+		    }
+		}
+	    }
+
+	    if (found == 0) {
+		printf("\nThere is no restaurant in the system by that name\n\n"); 
+	    }
+
+	    // write to fie
+	    fclose(restaurantsFile);
+	    restaurantsFile = fopen("./restaurants.txt", "w");
+	    for (int index = 0; index < insertIndex; index++) {
+		fprintf(restaurantsFile, "%s\n", restaurantsArrayCopy[index]);
+	    }
+	    
+	    fclose(restaurantsFile);
+	    free(restaurant);
+	}
+	// Select restaurant mode
         else if (strcmp(inputCopy, "p") == 0) {
             printf("\nThe restaurant you will go to is %s.\n\n", restaurantsArrayCopy[getRandom(0, insertIndex-1)]);
         }
+
+	// Quit mode
         else if (strcmp(inputCopy, "q") == 0) {
             printf("\nExiting program...\n");
             break;
